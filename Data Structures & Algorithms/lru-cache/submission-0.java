b908755759af1bbdab811ebb50c2889d417db103
Raghava S Ayyaji <raghavaayyaji@gmail.com>
2026-05-06 10:41:13 +0530

    Add: lru-cache - submission-0

---
+++ Data Structures & Algorithms/lru-cache/submission-0.java
@@ -0,0 +1,21 @@
+class LRUCache {
+    LinkedHashMap<Integer,Integer> map ;
+    int capacity;
+    public LRUCache(int capacity) {
+        this.capacity=capacity;
+      map=new LinkedHashMap(capacity,0.75f,true);
+    }
+    
+    public int get(int key) {
+       return map.getOrDefault(key,-1);
+    }
+    
+    public void put(int key, int value) {
+       map.put(key,value);
+       if(map.size()>capacity)
+       {
+        int o=map.keySet().iterator().next();
+        map.remove(o);
+       }
+    }
+}
